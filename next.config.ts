e032d58fde071507d5b2dce42d37353e382349ff
MaxDkn@Perelman <max.deckmyn@gmail.com>
2025-06-16 14:37:38 +0200

    deploy is working, css & js are linked with index.html => everything is correct

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  reactStrictMode: true,
   images: {
     unoptimized: true
   },
-  basePath: '',
-  assetPrefix: './'
 };
 
 export default nextConfig;
